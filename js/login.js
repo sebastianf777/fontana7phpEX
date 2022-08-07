4b67de63737b63722011a0742a6f74cc500b33a5
sebastianf777 <sebastianf777@outlook.com>
2022-08-07 18:19:29 -0300

    cleaning code

--- js/login.js
+++ js/login.js
@@ -31,24 +31,4 @@ let error_login = document.getElementById('error_login');
     }
 })
 
-    //$("input:text:visible:first").focus();
-    // let state = false;
-    // $('#accesspanel').on('submit', function(e) {
-
-    //     e.preventDefault();
-
-    //     state = !state;
-
-    //     if (state) {
-    //         document.getElementById("litheader").className = "poweron";
-    //         document.getElementById("go").className = "";
-    //         document.getElementById("go").value = "Initializing...";
-    //     }else{
-    //         document.getElementById("litheader").className = "";
-    //         document.getElementById("go").className = "denied";
-    //         document.getElementById("go").value = "Access Denied";
-    //     }
-
-    // });
-
 });
